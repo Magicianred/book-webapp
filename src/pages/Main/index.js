1855ff6b3c21bc2d07fdea26bbd491b1084590fe
Kamilla <kamilla.rodrigues@auvo.com.br>
2021-04-24 01:09:48 -0300

    Teste leitor de codigo de barras

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Quagga from 'quagga';
 
-import { Container } from './styles';
+import { Video } from './styles';
 
 function Main() {
-  return (
-    <Container>
-      <img
-        src="https://devsamurai-materials.s3.amazonaws.com/templates/dev-samurai-white.png"
-        height="256"
-        alt="Dev Samurai"
-      />
-      <h1>Fala Samurai!</h1>
-      <p>Esse é o template básico da Dev Samurai para React.</p>
-    </Container>
-  );
+  const onDetected = result => {
+    Quagga.offDetected(onDetected);
+
+    const isbn = result.codeResult.code;
+    alert(isbn);
+  }
+
+  useEffect(() => {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      Quagga.init({
+        inputStream: {
+          name: "Live",
+          type: "LiveStream",
+          target: document.querySelector("#video"),
+          constraints: {
+            facingMode: 'environment',
+          },
+        },
+        numOfWorkers: 1,
+        locate: true,
+        decoder: {
+          readers: ['ean_reader'],
+        },
+      },
+        error => {
+          if (error) {
+            console.error(error);
+            alert("Erro ao abrir a câmera do dispositivo");
+            return;
+          }
+          Quagga.start();
+        },
+        Quagga.onDetected(onDetected)
+      );
+    }   
+  }, []);
+
+return (
+  <Video id="video" />
+);
 }
 
 export default Main;
